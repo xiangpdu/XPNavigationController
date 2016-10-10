9ca41bd026411b7614bce776484125aaa8b8d682
xpdu <443173414@qq.com>
2016-10-10 22:32:11 +0800

    Initial Commit

---
+++ XPNavigationController/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  XPNavigationController
+//
+//  Created by duxiangping on 2016/10/10.
+//  Copyright © 2016年 duxiangping. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
